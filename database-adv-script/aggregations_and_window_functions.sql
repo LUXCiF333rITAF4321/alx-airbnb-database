e2aabf4732f007e04a4366c0d1f8367fc19ad904
saad333 <saadouddah1@gmail.com>
2025-09-08 02:59:25 +0100

    fix: add ROW_NUMBER() to window function query

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -12,13 +12,14 @@ GROUP BY u.user_id, u.first_name, u.last_name
 ORDER BY total_bookings DESC;
 
 -- =========================================
--- Window Function: rank properties by total number of bookings
+-- Window Functions: rank properties by total number of bookings
 -- =========================================
 SELECT
     p.property_id,
     p.name AS property_name,
     COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property_rank
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property_rank,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS row_number_position
 FROM Property p
 LEFT JOIN Booking b ON p.property_id = b.property_id
 GROUP BY p.property_id, p.name
